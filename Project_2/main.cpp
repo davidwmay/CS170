dd35e72cc8845f94239ffa3381b5739f2e66eb64
David May <davidmay@Davids-MacBook-Pro.local>
2019-12-13 04:27:49 -0800

    Backward elimination algorithm likely working

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 vector<double> loadData;
 
@@ -82,7 +83,6 @@ float leave_one_out(vector<node> data, vector<int> featureSet, int featureToAdd)
                     distance += pow( data.at(i).features.at(featureToAdd) - data.at(j).features.at(featureToAdd), 2);
 
                 distance = sqrt(distance);
-                // cout << "Distance: " << setprecision(10) << distance << endl;
 
                 if (distance < bestSoFar) {
                     bestSoFar = distance;
@@ -91,16 +91,46 @@ float leave_one_out(vector<node> data, vector<int> featureSet, int featureToAdd)
             }
         }
 
-        // cout << "For exemplar " << i << " I think its nearest neighbor is " << bestSoFarLoc << endl << endl;
         if (data.at(i).classification == data.at(bestSoFarLoc).classification) {
-            // cout << "I got exemplar " << i << " correct.\n\n";
             numCorrect++;
         } 
     }
-    // cout << "float(numCorrect) = " << float(numCorrect) << endl;
-    // cout << "(data.size() - 1) = " << data.size() << endl;
     accuracy = float(numCorrect) / data.size();
-    // cout << "The accuracy of this test was " << setprecision(10) << accuracy * 100 << "%" << endl << endl;
+    return accuracy;
+}
+
+float leave_one_out_backward(vector<node> data, vector<int> featureSet, int featureToRemove) {
+    int numCorrect = 0;
+    float accuracy = 0;
+
+    for (int i = 0; i < data.size(); i++) {
+        double bestSoFar = INT_MAX;
+        int bestSoFarLoc = 0;
+
+        for (int j = 0; j < data.size(); j++) {
+            double distance = 0;
+            if (i != j) {
+
+                for (int k = 0; k < featureSet.size(); k++) {
+                    if (k != featureToRemove) {
+                        distance += pow( data.at(i).features.at(featureSet.at(k)) - data.at(j).features.at(featureSet.at(k)), 2);
+                    }
+                }
+
+                distance = sqrt(distance);
+
+                if (distance < bestSoFar) {
+                    bestSoFar = distance;
+                    bestSoFarLoc = j;
+                }
+            }
+        }
+
+        if (data.at(i).classification == data.at(bestSoFarLoc).classification) {
+            numCorrect++;
+        } 
+    }
+    accuracy = float(numCorrect) / data.size();
     return accuracy;
 }
 
@@ -123,8 +153,9 @@ void forward_selection(vector<node> data) {
                 for (int x = 0; x < featureSet.size(); x++) {
                     cout << featureSet.at(x) + 1 << ", ";
                 }
+
                 cout << j + 1;
-                cout << "} accuracy is " << accuracy * 100 << "%\n";
+                cout << "} accuracy is " << setprecision(3) << accuracy * 100 << "%\n";
 
                 if (accuracy > bestSoFar) {
                     bestSoFar = accuracy;
@@ -147,22 +178,112 @@ void forward_selection(vector<node> data) {
         for (int x = 0; x < featureSet.size() - 1; x++) {
             cout << featureSet.at(x) + 1 << ", ";
         }
-        cout << featureSet.at(featureSet.size() - 1);
-        cout << "} was best, accuracy is " << setprecision(10) << bestSoFar * 100 << "%\n\n";
+        cout << featureSet.at(featureSet.size() - 1) + 1;
+        cout << "} was best, accuracy is " << setprecision(3) << bestSoFar * 100 << "%\n\n";
     }
 
     cout << "Finished search. The best feature subset is {";
     for (int i = 0; i < bestFeatures.size() - 1; i++) {
         cout << bestFeatures.at(i) + 1 << ", ";
     }
-    cout << bestFeatures.at(bestFeatures.size() - 1) + 1 << "}, which has an accuracy of " << overallBest * 100 << "%\n\n";
+    cout << bestFeatures.at(bestFeatures.size() - 1) + 1 << "}, which has an accuracy of " << setprecision(3) << overallBest * 100 << "%\n\n";
+}
+
+void backward_elimination(vector<node> data) {
+    vector<int> featureSet;
+    vector<int> bestFeatures;
+    float overallBest = 0;
+    bool decrease = 0;
+    bool outputCheck = 0;
+    cout << "Beginning search.\n\n";
+
+    for (int i = 0; i < data.at(0).features.size(); i++) {
+        featureSet.push_back(i);
+    }
+
+    cout << "Starting feature set: ";
+    for (int i = 0; i < featureSet.size(); i++) {
+        cout << featureSet.at(i) + 1 << " ";
+    }
+    cout << "\n\n";
+
+    for (int i = 0; i < data.at(0).features.size(); i++) {
+        int featureToRemove = 0;
+        float bestSoFar = 0;
+
+        for (int j = 0; j < data.at(0).features.size(); j++) {
+            if (find(featureSet.begin(), featureSet.end(), j) != featureSet.end() && featureSet.size() > 1) {
+                float accuracy = leave_one_out_backward(data, featureSet, j);
+                cout << "   Using feature(s) {";
+
+                for (int x = 0; x < featureSet.size() - 2; x++) {
+                    if (featureSet.at(x) != j) {
+                        cout << featureSet.at(x) + 1 << ", ";
+                    }
+                }
+                if (featureSet.at(featureSet.size() - 2) == j) {
+                    cout << featureSet.at(featureSet.size() - 1) + 1;
+                } else if (featureSet.at(featureSet.size() - 1) == j) {
+                    cout << featureSet.at(featureSet.size() - 2) + 1;
+                } else {
+                    cout << featureSet.at(featureSet.size() - 2) + 1 << ", ";
+                    cout << featureSet.at(featureSet.size() - 1) + 1;
+                }
+                // if (featureSet.at(featureSet.size() - 1 != j)) {
+                //     cout << featureSet.at(featureSet.size() - 1) + 1;
+                // } 
+                cout << "} accuracy is " << setprecision(3) << accuracy * 100 << "%\n";
+
+                if (accuracy > bestSoFar) {
+                    bestSoFar = accuracy;
+                    featureToRemove = j;
+                }
+            }
+        }
+
+        cout << "\n";
+
+        for (int i = 0; i < featureSet.size(); i++) {
+            if (featureSet.at(i) == featureToRemove) {
+                featureSet.erase(featureSet.begin() + i);
+                break;
+            }
+        }
+
+        if (bestSoFar > overallBest) {
+            overallBest = bestSoFar;
+            bestFeatures = featureSet;
+        } else if (bestSoFar < overallBest && !decrease) {
+            cout << "WARNING: Accuracy has decreased. Continuing search in case of local maxima.\n\n";
+            decrease = 1;
+        }
+
+        if (featureSet.size() != 1 || (featureSet.size() == 1 && !outputCheck)) {
+            if (featureSet.size() == 1) {
+                outputCheck = 1;
+            }
+            cout << "Feature set {";
+            for (int x = 0; x < featureSet.size() - 1; x++) {
+                cout << featureSet.at(x) + 1 << ", ";
+            }
+            cout << featureSet.at(featureSet.size() - 1) + 1;
+            cout << "} was best, accuracy is " << setprecision(10) << bestSoFar * 100 << "%\n\n";
+        }
+
+    }
+
+    cout << "Finished search. The best feature subset is {";
+    for (int i = 0; i < bestFeatures.size() - 1; i++) {
+        cout << bestFeatures.at(i) + 1 << ", ";
+    }
+    cout << bestFeatures.at(bestFeatures.size() - 1) + 1 << "} which has an accuracy of " << overallBest * 100 << "%\n\n";
 
 }
 
 int main() {
     vector<node> data;
     string fileName;
-    string algChoice;
+    int algChoice;
     ifstream inFile;
     double temp;
 
@@ -171,11 +292,11 @@ int main() {
 
     // 2 lines below for testing only
     fileName = "SMALL70.txt";
-    // cout << "Type the number of the algorithm you want to run: " << endl;
-    // cout << "   1) Forward Selection" << endl;
-    // cout << "   2) Backward Elimination" << endl;
-    // cout << "   3) Custom Algorithm" << endl;
-    // cin >> algChoice;
+    cout << "Type the number of the algorithm you want to run: " << endl;
+    cout << "   1) Forward Selection" << endl;
+    cout << "   2) Backward Elimination" << endl;
+    cout << "   3) Custom Algorithm" << endl;
+    cin >> algChoice;
 
     inFile.open(fileName);
 
@@ -199,7 +320,14 @@ int main() {
     }
 
     normalize(data);
-    forward_selection(data);
+    
+    if (algChoice == 1) {
+        forward_selection(data);
+    } else if (algChoice == 2) {
+        backward_elimination(data);
+    } else {
+        // custom algorithm
+    }
     //vector below for testing
     // vector<int> featureTest;
     // featureTest.push_back(4);
